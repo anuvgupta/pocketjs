055d69d5843cb1bd787afab5e236b89860f3a0a0
Anuv Gupta <anuv.ca@gmail.com>
2016-07-15 04:33:18 -0700

    finished functional chat demo

--- demo/www/index.php
+++ demo/www/index.php
@@ -1,51 +1,55 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <style type = 'text/css'>
-            body {
-                background-color: #111111;
-            }
-            #output {
-                height: 400px;
-                width: 500px;
-                background-color: #555555;
-                margin: 8px;
-                padding: 8px 15px 8px 15px;
-                font: 20px Arial, sans-serif;
-                color: #EEEEEE;
-            }
-            #message {
-                width: 500px;
-                overflow-x: hidden;
-                word-wrap: break-word;
-            }
-            #input {
-                text-align: center;
-            }
-        </style>
+        <link rel = 'stylesheet' type = 'text/css' href = './style.css'/>
         <script type = 'text/javascript' src = './pocket.js'></script>
         <script type = 'text/javascript'>
             var element = function (id) { return document.getElementById(id); };
+            var name = '';
             window.addEventListener('load', function () {
                 var send = function () {
-                    Pocket.send('message', Pocket.getID(), element('input').value);
+                    var val = element('input').value;
+                    element('input').value = '';
+                    if ((val !== null) && (val != '') && val != ' ') {
+                        if ((name == null) || (name == '') || (name == ' ')) Pocket.send('username', val);
+                        else Pocket.send('message', val);
+                    }
                 };
                 element('submit').addEventListener('click', send);
                 element('input').addEventListener('keyup', function (e) {
                     if ((e.keyCode ? e.keyCode : e.which) == '13') send();
                 });
-                Pocket.bind('message', function (user, message) {
+                Pocket.bind('username', function(success, username) {
+                    if (success) {
+                        element('output').style.height = (0.7 * window.innerHeight) + 'px';
+                        element('output').style.backgroundColor = '#222222';
+                        element('output').style.borderStyle = 'solid';
+                        element('output').style.borderBottomWidth = '45px';
+                        element('input').style.borderRadius = '0 0 0 10px';
+                        element('input').placeholder = 'message';
+                        element('submit').style.borderRadius = '0 0 10px 0';
+                        element('submit').innerHTML = 'send';
+                        name = username;
+                        Pocket.send('ready');
+                    } else console.log('Username not accepted');
+                });
+                Pocket.bind('message', function (user, message, id) {
                     element('output').innerHTML += "<span id = 'message'><strong>" + user + '</strong>: ' + message + '</span><br/>';
+                    element('output').scrollTop += 50;
                 });
                 Pocket.connect('localhost', 30000, 'demo/server.php');
             });
         </script>
+        <title>pocketjs chat</title>
     </head>
     <body>
-        <center>
+        <center id = 'content'>
+            <span id ='title'>PocketJS Chat</span>
             <div id = 'output'></div>
-            <input id = 'input' type = 'text' placeholder = 'Message'/>
-            <button id = 'submit'>Send</button>
+            <div id = 'inputwrap'>
+                <input id = 'input' type = 'text' placeholder = 'nickname'/>
+                <button id = 'submit'>join</button>
+            </div>
         </center>
     </body>
 </html>
