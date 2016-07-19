b05f7f1cd499b9f7c0a7ed16d3264d5469e3958d
anuvgupta <anuv.ca@gmail.com>
2016-07-19 02:51:18 -0700

    updated scroll animation

--- js/app.js
+++ js/app.js
@@ -3,9 +3,11 @@ var _ = function (id) { return document.getElementById(id); };
 
 function load() {
     _('main').style.paddingRight = (_('main').offsetWidth - _('main').clientWidth) + 'px';
+    var hookTop = $('#hook').offset().top;
     $('#intro .nav .image').click(function () {
         $('#main').animate({
-            scrollTop: ($('#hook').offset().top) + 'px'
+            //scrollTop: Math.abs($('#hook').offset().top - $('#main').scrollTop()) + 'px'
+            scrollTop: hookTop + 'px'
         }, 700);
     });
 }
