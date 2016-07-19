4c07a4be364052c85c18f918e52cdac5a0ab01b1
anuvgupta <anuv.ca@gmail.com>
2016-07-19 02:59:20 -0700

    final scroll fix

--- js/app.js
+++ js/app.js
@@ -3,11 +3,9 @@ var _ = function (id) { return document.getElementById(id); };
 
 function load() {
     _('main').style.paddingRight = (_('main').offsetWidth - _('main').clientWidth) + 'px';
-    var hookTop = $('#hook').offset().top;
     $('#intro .nav .image').click(function () {
         $('#main').animate({
-            //scrollTop: Math.abs($('#hook').offset().top - $('#main').scrollTop()) + 'px'
-            scrollTop: hookTop + 'px'
+            scrollTop: $('#hook').offset().top + $('#main').scrollTop() + 'px'
         }, 700);
     });
 }
