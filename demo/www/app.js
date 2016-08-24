45613f938d8b90d114292649a9a9d81524f05fe6
Anuv Gupta <anuv.ca@gmail.com>
2016-08-23 20:30:37 -0700

    minor changes

---
+++ demo/www/app.js
@@ -0,0 +1,39 @@
+var element = function (id) { return document.getElementById(id); };
+var name = '';
+window.addEventListener('load', function () {
+    var send = function () {
+        var val = element('input').value;
+        element('input').value = '';
+        if ((val !== null) && (val != '') && val.trim() != '') {
+            if ((name == null) || (name == '') || (name.trim() == ''))
+                Pocket.send('username', val);
+            else Pocket.send('message', val);
+        }
+    };
+    element('submit').addEventListener('click', send);
+    element('input').addEventListener('keyup', function (e) {
+        if ((e.keyCode ? e.keyCode : e.which) == '13') send();
+    });
+    Pocket.bind('username', function(success, username) {
+        if (success) {
+            element('output').style.height = (0.66 * window.innerHeight) + 'px';
+            setTimeout(function () {
+                element('output').style.overflowY = 'auto';
+            }, 500);
+            element('output').style.backgroundColor = '#222222';
+            element('output').style.borderStyle = 'solid';
+            element('output').style.borderBottomWidth = '45px';
+            element('input').style.borderRadius = '0 0 0 10px';
+            element('input').placeholder = 'message';
+            element('submit').style.borderRadius = '0 0 10px 0';
+            element('submit').innerHTML = 'send';
+            name = username;
+            Pocket.send('ready');
+        } else console.log('Username not accepted');
+    });
+    Pocket.bind('message', function (user, message, id) {
+        element('output').innerHTML += "<span id = 'message'><strong>" + user + '</strong>: ' + message + '</span><br/>';
+        element('output').scrollTop += 50;
+    });
+    Pocket.connect('aws.anuv.me', 8000, 'chat.php');
+});
