84cbe86a1b114d86bed76701fcb23dbe409a0166
Anuv Gupta <anuv.ca@gmail.com>
2016-09-07 00:24:20 -0700

    update mobile sizing

--- app.js
+++ app.js
@@ -72,14 +72,12 @@ body = Block('div', 'app')
         )
         .add(Block('block', 'links')
             .id('links')
-            .css('top', (window.innerHeight * 2.05 + 400) + 'px')
             .add('link button', 'demo')
             .add('link button', 'docs')
             .add('link button', 'source')
         )
         .add(Block('div', 'contact')
             .id('contact')
-            .css('top', (window.innerHeight * 2.05 + 400 + 100) + 'px')
             .add(Block('block', 'main')
                 .add(Block('block', 'author')
                     .add(Block('div', 'link')
@@ -138,11 +136,27 @@ body = Block('div', 'app')
 
 
 $(document).ready(function () {
+    // function for resizing elements
+    var initialSize = window.innerHeight;
+    var mobileAgent = function () {
+        var check = false;
+        (function (a) { if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+        return check;
+    };
+    var mobileTabletAgent = function () {
+        var check = false;
+        (function (a) { if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+        return check;
+    };
     var size = function () {
+        if (mobileTabletAgent())
+            window.innerHeight = initialSize;
+        // hide scrollbar
         var _main = body.child('main').node();
         _main.style.paddingRight = (_main.offsetWidth - _main.clientWidth) + 'px';
-        var intro = body.child('main/intro');
-        intro
+        // page positioning
+        var introTop = mobileAgent() ? window.innerHeight * 2.125 : window.innerHeight * 2.05;
+        body.child('main/intro')
             .css({
                 top: '0',
                 height: window.innerHeight + 'px'
@@ -154,66 +168,35 @@ $(document).ready(function () {
             })
         .sibling('info')
             .css({
-                top: (window.innerHeight * 2.05) + 'px',
+                top: (introTop) + 'px',
                 height:  '400px'
             })
         .sibling('links')
             .css({
-                top: (window.innerHeight * 2.05 + 400) + 'px',
+                top: (introTop + 400) + 'px',
                 height:  '100px'
             })
         .sibling('contact')
             .css({
-                top: (window.innerHeight * 2.05 + 400 + 100) + 'px',
+                top: (introTop + 400 + 100) + 'px',
                 height:  '400px'
             })
         ;
-        var reset = function () {
-            intro.child('logo')
-                .css('height', (0.48 * window.innerHeight) + 'px')
-                .css('width', (0.48 * window.innerHeight) + 'px')
-            .sibling('nav')
-                .css('display', 'block')
-            .sibling('title')
-                .css('font-size',  '138px')
-            .sibling('text')
-                .css('font-size', '23px')
-                .child('space').html(' ')
-            ;
-            body.child('main/contact/footer/breakB')
-                        .html(' ')
-                    .sibling('breakC')
-                        .html('')
-                    .parent()
-                .css('bottom', '1px')
-                .sibling('main')
-                    .css('padding-bottom', '0px')
-                    .parent()
-                .css({
-                    top: (window.innerHeight * 2.05 + 400 + 100) + 'px',
-                    height: '400px'
-                })
-                .sibling('links')
-                    .css('height', '100px')
-            ;
-            var children = body.child('main/links').children();
-            for (var i in children)
-                children[i].on('reg');
-        };
-        var phone = function () {
-            // return (window.innerHeight > window.innerWidth) || (window.innerWidth < 700);
-            return (window.innerWidth < 700);
-        };
-        if (phone()) {
-            intro.child('logo')
+
+        // width based resizing
+        if (window.innerWidth < 700) {
+            // switch to mobile view
+            body.child('main/intro/logo')
                 .css('height', (0.43 * window.innerHeight) + 'px')
                 .css('width', (0.43 * window.innerHeight) + 'px')
             .sibling('title')
                 .css('font-size',  '118px')
             .sibling('text')
                 .css('font-size', '22.5px')
-                .child('space').html('<br/>');
-                body.child('main/contact/footer/breakB')
+                .child('space')
+                    .html('<br/>')
+            ;
+            body.child('main/contact/footer/breakB')
                             .html('<br/>')
                         .sibling('breakC')
                             .html('<br/>')
@@ -222,17 +205,81 @@ $(document).ready(function () {
                     .sibling('main')
                         .css('padding-bottom', '20px')
                         .parent()
-                        .css({
-                            top: (window.innerHeight * 2.05 + 400 + 280) + 'px',
-                            height: '410px'
-                        })
-                    .sibling('links')
-                        .css('height', '280px')
-                ;
-                var children = body.child('main/links').children();
-                for (var i in children)
-                    children[i].on('alt');
-        } else reset();
+                    .css({
+                        top: (introTop + 400 + 280) + 'px',
+                        height: '410px'
+                    })
+                .sibling('links')
+                    .css('height', '280px')
+            ;
+            var children = body.child('main/links').children();
+            for (var i in children)
+                children[i].on('alt');
+        } else {
+            // or switch to regular view
+            body.child('main/intro/logo')
+                .css('height', (0.48 * window.innerHeight) + 'px')
+                .css('width', (0.48 * window.innerHeight) + 'px')
+            .sibling('nav')
+                .css('display', 'block')
+            .sibling('title')
+                .css('font-size',  '138px')
+            .sibling('text')
+                .css('font-size', '23px')
+                .child('space')
+                    .html(' ')
+            ;
+            body.child('main/contact/footer/breakB')
+                            .html(' ')
+                        .sibling('breakC')
+                            .html('')
+                        .parent()
+                    .css('bottom', '1px')
+                    .sibling('main')
+                        .css('padding-bottom', '0px')
+                    .parent()
+                .css({
+                    top: (introTop + 400 + 100) + 'px',
+                    height: '400px'
+                })
+                .sibling('links')
+                    .css('height', '100px')
+            ;
+            var children = body.child('main/links').children();
+            for (var i in children)
+                children[i].on('reg');
+        }
+
+        // height based resizing
+        if (window.innerHeight < 600) {
+            body.child('main/intro/nav/text')
+                    .css('display', 'none')
+                .parent()
+            .css({
+                height: '50px',
+                bottom: '12px'
+            });
+        } else {
+            body.child('main/intro/nav/text')
+                    .css('display', 'inline-block')
+                .parent()
+            .css({
+                height: '70px',
+                bottom: '12px'
+            });
+        }
+        if (window.innerHeight < 550)
+            body.child('main/intro/nav/down').css('transform', 'scale(0.8, 0.8)');
+        else body.child('main/intro/nav/down').css('transform', 'scale(1, 1)');
+        if (window.innerHeight < 450)
+            body.child('main/intro/nav').css('display', 'none');
+        else body.child('main/intro/nav').css('display', 'block');
+
+        // mobile resizing
+        if (mobileAgent()) {
+            body.child('main/intro/title').css('font-size', '80px');
+            body.child('main/intro/text').css('font-size', '12px');
+        }
     }
 
     // make down arrow disappear when scrolled too far
@@ -243,26 +290,31 @@ $(document).ready(function () {
     //     else nav.css('opacity', '1');
     // });
 
-    // convenience function for scrolling to div by id
-    var scroll = function () {
-        var index = window.location.href.indexOf('#');
-        if (index == -1) return false;
-        var id = window.location.href.substring(index + 1);
-        if (document.getElementById(id) == null) return false;
-        $(document.body).animate({
-            scrollTop: $('#' + id).offset().top + 'px'
-        }, 700);
-    };
-    // load blockfile
+    // load blockfile with jQuery
     body.load(function (b) {
+        // load blocks into body
         b.fill(document.body);
-        size();
+        // resize on window resize
         $(window).resize(size);
+        size(); // initial sizing
         setTimeout(function () {
+            // display body
             b.css('opacity', '1');
-            scroll();
+            // fix background color
+            setTimeout(function () {
+                document.body.style.backgroundColor = '#262626';
+            }, 400);
+            // scroll to id in url
+            var index = window.location.href.indexOf('#');
+            if (index == -1) return false;
+            var id = window.location.href.substring(index + 1);
+            if (document.getElementById(id) == null) return false;
+            $(document.body).animate({
+                scrollTop: $('#' + id).offset().top + 'px'
+            }, 700);
         }, 10);
     }, 'app', 'jQuery');
+
     // load code html
     $.ajax({
         url: 'code/php.txt',
--- js/blocks.js
+++ js/blocks.js
@@ -1,68 +1,59 @@
-// custom blocks break, text, and image
-
-// define break block
-Block('break', function () { //function to create break block
-    var block = Block('span') //start off with a span block
-        .add(Block('br')); //add a line break to the span
-    return block; //return the newly modified block
-}, function (block, data) { //function to load data into break block
-    var value = data('val'); //get block data 'val' (amount of breaks)
-    if (value !== null) { //if val is null, don't change block
-        //else add that many extra line breaks
+Block('break', function () {
+    var block = Block('span')
+        .add(Block('br'));
+    return block;
+}, function (block, data) {
+    var value = data('val');
+    if (value !== null) {
         for (var i = 1; i < value; i++) block.add(Block('br'));
     }
-    //prevent blocks from being added to this block
     block.add = function () {
-        return block; //return block to allow chaining
+        return block;
     };
 });
 
-// define text block
-Block('text', function () { //function to create text block
-    var block = Block('span'); //start off with a span block
-    //until data is loaded, span is blank, so do nothing
-    return block; //return the newly modified block
-}, function (block, data) { //function to load data into text block
-    var value = data('val'); //get data 'val' (text of span)
-    // if val is not null, add text to text block
+Block('text', function () {
+    var block = Block('span');
+    return block;
+}, function (block, data) {
+    var value = data('val');
     if (value != null)
         block.node().appendChild(document.createTextNode(value.replace(/&nbsp;/g, ' ')));
 });
 
-// define image block
-Block('image', function () { //function to create image block
-    var block = Block('div'); //start off with div block
-    block.css('opacity', '0'); //until data is loaded, image is blank, so hide
-    return block; //return the newly modified block
-}, function (block, data, css) { //function to load data into image block
-    //prevent blocks from being added to this block
+Block('image', function () {
+    var block = Block('div');
+    block.css('opacity', '0');
+    return block;
+}, function (block, data, css) {
     block.add = function () {
-        return block; //return block to allow chaining
+        return block;
     };
-    var element = block.node(); //get the block's element
-    var src = data('src'); //get data 'src' (image source)
-    var height = data('height'); //get data 'height'
-    var width = data('width'); //get data 'width'
-    if (src != null) { //if src is not null
-        //load background image
+    var element = block.node();
+    var src = data('src');
+    var height = data('height');
+    var width = data('width');
+    if (src != null) {
         var img = new Image();
         img.onload = function () {
             var c = document.createElement('canvas');
             c.width = this.naturalWidth;
             c.height = this.naturalHeight;
             c.getContext('2d').drawImage(this, 0, 0);
-            element.style.backgroundImage = "url('" + c.toDataURL('image/png') + "')";
-            if (css('opacity') != null) element.style.opacity = css('opacity');
-            else element.style.opacity = 1;
+            block.css('background-image', "url('" + c.toDataURL('image/png') + "')");
+            setTimeout(function () {
+                if (css('opacity') != null) element.style.opacity = css('opacity');
+                else block.css('opacity', '1');
+            }, 10);
         };
         img.src = src;
-        element.style.backgroundRepeat = 'no-repeat';
-        element.style.backgroundSize = 'contain';
+        block.css({
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'contain'
+        });
     }
-    //if height is not null, set image div height
     if (height !== null) element.style.height = height;
     else element.style.height = 'auto';
-    //if width is not null, set image div width
     if (width !== null) element.style.width = width;
     else element.style.width = 'auto';
 });
@@ -111,7 +102,8 @@ Block('panel', function () {
                 .css({
                     display: 'block',
                     paddingBottom: '6px',
-                    fontSize: '19px'
+                    fontSize: '19px',
+                    color: '#cdcdcd'
                 })
                 .data(title)
         ;
